da4f6ca591e0c49d668ebe9afa801e80989d03b0
Daniel Heydari <danielheydari78@gmail.com>
2023-03-08 15:57:56 +0330

    categorybox item fixed width

--- components/category/CategoryBox.jsx
+++ components/category/CategoryBox.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const CategoryBox = ({ name, discount, thumbnail }) => {
   return (
     <>
-      <div className={` bg-white  p-2 rounded-md max-w-max border-2  `}>
+      <div className={` bg-white  p-2 rounded-md w-full border-2  `}>
         <a href="#">
           <figure className="flex gap-3 items-center justify-start">
             <Image className="" width={70} height={15} src={thumbnail} alt="category" />
@@ -12,7 +12,7 @@ const CategoryBox = ({ name, discount, thumbnail }) => {
               <h3 className="flex  items-start flex-col gap-3 tracking-tighter">
                 <span className="text-md ">{name}</span>
                 <div
-                  className={` p-2 rounded text-center text-xs text-success bg-green-100 hover:bg-green-200`}
+                  className={` p-2 text-center text-xs text-success bg-green-100 hover:bg-green-200`}
                 >
                   <span className="font-normal ">تخفیف تا </span>
                   <strong >{discount}%</strong>
